c9a9cb5b24ecc4bd2ed23503c00c6a6882fa88d3
your username <nasrimehdi96@gmail.com>
2020-11-16 12:15:47 +0100

    first commit

---
+++ src/test/java/org/tekup/TekupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tekup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
